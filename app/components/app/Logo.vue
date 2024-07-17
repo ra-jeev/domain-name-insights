ad1ed6f32e63594982b9bdc18deb4e55483e02a8
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-17 23:34:08 +0530

    feat Add basic app logo

---
+++ app/components/app/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <ULink to="/" class="flex items-center whitespace-nowrap gap-2">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="iconSize"
+      :height="iconSize"
+      viewBox="0 0 24 24"
+    >
+      <path
+        class="fill-primary"
+        d="M21 8c-1.45 0-2.26 1.44-1.93 2.51l-3.55 3.56c-.3-.09-.74-.09-1.04 0l-2.55-2.55C12.27 10.45 11.46 9 10 9c-1.45 0-2.27 1.44-1.93 2.52l-4.56 4.55C2.44 15.74 1 16.55 1 18c0 1.1.9 2 2 2c1.45 0 2.26-1.44 1.93-2.51l4.55-4.56c.3.09.74.09 1.04 0l2.55 2.55C12.73 16.55 13.54 18 15 18c1.45 0 2.27-1.44 1.93-2.52l3.56-3.55c1.07.33 2.51-.48 2.51-1.93c0-1.1-.9-2-2-2"
+      />
+      <path
+        class="fill-amber-500"
+        d="m15 9l.94-2.07L18 6l-2.06-.93L15 3l-.92 2.07L12 6l2.08.93zM3.5 11L4 9l2-.5L4 8l-.5-2L3 8l-2 .5L3 9z"
+      />
+    </svg>
+    <div :class="textSize" class="leading-none font-bold">UrlInsight</div>
+  </ULink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  textSize: {
+    type: String,
+    default: "text-2xl",
+  },
+  iconSize: {
+    type: Number,
+    default: 36,
+  },
+});
+</script>
