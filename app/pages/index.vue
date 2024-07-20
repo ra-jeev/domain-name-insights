d690eba1c620b0b470f2d29d7a409b7de9a5ceb6
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-20 23:18:09 +0530

    feat: basic index & dynamic insights page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="p-4 flex items-center justify-center h-[500px]">
-    <DomainNameInput />
+  <div
+    class="p-4 flex flex-col gap-4 items-center justify-center min-h-[500px]"
+  >
+    <DomainNameInput @name-input="onNameInput" />
   </div>
 </template>
+
+<script setup lang="ts">
+const onNameInput = async (name: string) => {
+  console.log(name);
+  navigateTo(`/insights/${name}`);
+};
+</script>
---
+++ app/pages/insights/[domainName].vue
@@ -0,0 +1,16 @@
+<template>
+  <UContainer
+    class="py-4 flex flex-col gap-4 items-center justify-center min-h-[500px]"
+  >
+    <div v-if="status === 'pending'">Loading ...</div>
+    <div v-else class="max-w-lg whitespace-pre text-pretty">
+      {{ JSON.stringify(insights, null, 4) }}
+    </div>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+const { data: insights, status } = await useLazyFetch(
+  `/api/insights/${useRoute().params.domainName}`
+);
+</script>
