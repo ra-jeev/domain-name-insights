157caac33a4d2e208cef967523a52e58baa09def
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-22 21:52:05 +0530

    feat: create DomainScoreLoader component

---
+++ app/components/DomainScoreLoader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="space-y-6">
+    <OverallScoreLoader />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <CategoryScoreLoader v-for="n in 6" :key="`category-loader-${n}`" />
+    </div>
+  </div>
+</template>
