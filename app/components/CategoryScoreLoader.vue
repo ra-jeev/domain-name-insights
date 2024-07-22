9d64e4994af1faa7543ab192ce3912de21721a66
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-22 21:51:06 +0530

    feat: create CategoryScoreLoader component

---
+++ app/components/CategoryScoreLoader.vue
@@ -0,0 +1,11 @@
+<template>
+  <UCard>
+    <div class="flex items-center gap-x-4 md:gap-x-6">
+      <ScoreMeter :percentage="0" :size="60" />
+      <div>
+        <USkeleton class="h-5 w-28 mb-2" />
+        <USkeleton class="h-4 w-full min-w-40 mb-1" />
+      </div>
+    </div>
+  </UCard>
+</template>
