263dccb8fb0f265007e13090b33c184f3d0433d0
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-22 21:52:27 +0530

    feat: new CategoryScoreCard component

---
+++ app/components/CategoryScoreCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <UCard>
+    <div class="flex items-center gap-x-4 md:gap-x-6">
+      <ScoreMeter :percentage="data.score" :size="80" />
+      <div>
+        <h3 class="text-lg md:text-xl font-semibold mb-2">
+          {{ categoryName }}
+        </h3>
+        <p class="text-gray-500 dark:text-gray-400">{{ data.explanation }}</p>
+      </div>
+    </div>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+import { type CategoryScore } from "~~/types";
+
+defineProps<{
+  categoryName: string;
+  data: CategoryScore;
+}>();
+</script>
