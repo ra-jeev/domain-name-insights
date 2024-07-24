2ec669184482e932a6ffa0d82e67f5962972fa95
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-24 17:35:02 +0530

    feat: new FeatureCard component

---
+++ app/components/FeatureCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <UCard>
+    <UIcon :name="icon" class="w-10 h-10 mb-4 text-primary" />
+
+    <h3 class="text-xl font-semibold mb-2">{{ title }}</h3>
+
+    <p class="text-gray-600 dark:text-gray-300">
+      {{ description }}
+    </p>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+</script>
