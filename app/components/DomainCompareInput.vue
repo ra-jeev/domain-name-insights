e43e0f040837e9e28a9d2d756c04455c1f5631ff
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-25 12:27:02 +0530

    feat: New DomainCompareInput component

---
+++ app/components/DomainCompareInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="w-full max-w-md space-y-4" @submit.prevent="compareDomains">
+    <UInput
+      v-model.trim="domain1"
+      placeholder="Enter the first domain..."
+      size="lg"
+    />
+
+    <UInput
+      v-model.trim="domain2"
+      placeholder="Enter the second domain..."
+      size="lg"
+    />
+
+    <UButton
+      block
+      size="lg"
+      trailing-icon="i-heroicons-rocket-launch"
+      type="submit"
+    >
+      Compare Now
+    </UButton>
+  </form>
+</template>
+
+<script setup lang="ts">
+const domain1 = ref("");
+const domain2 = ref("");
+
+const compareDomains = () => {
+  if (!domain1.value || !domain2.value) {
+    alert("Please enter both the domain names");
+  } else if (!isValidDomain(domain1.value)) {
+    alert(`"${domain1.value}" is not a valid domain name`);
+  } else if (!isValidDomain(domain2.value)) {
+    alert(`"${domain2.value}" is not a valid domain name`);
+  } else {
+    navigateTo(
+      `/compare/${encodeURIComponent(domain1.value)},${encodeURIComponent(
+        domain2.value
+      )}`
+    );
+  }
+};
+</script>
