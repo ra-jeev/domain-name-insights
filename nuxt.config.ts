417f2e05de1492ef634339dec2179b428b066626
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-26 12:58:02 +0530

    feat: add simple icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,25 @@
 export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
   modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/ui"],
+
   hub: {
     database: true,
     kv: true,
     blob: true,
     cache: true,
   },
+
   nitro: {
     experimental: {
       openAPI: true,
     },
   },
+
   devtools: { enabled: true },
+
   ui: {
-    icons: ["mdi"],
+    icons: ["mdi", "simple-icons"],
   },
+
+  compatibilityDate: "2024-07-26",
 });
