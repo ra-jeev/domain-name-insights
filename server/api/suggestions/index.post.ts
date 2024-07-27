1ccb47ce836b30b11169a60b7f1b20b7adf80d0c
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-27 13:16:24 +0530

    feat: add suggestions api endpoint

---
+++ server/api/suggestions/index.post.ts
@@ -0,0 +1,39 @@
+import { getAIService } from "~~/server/services/AIServiceFactory";
+import type { DomainSuggestionsData } from "~~/types";
+
+const saveSuggestions = async (purpose: string, data: string) => {
+  const db = hubDatabase();
+  const res = await db
+    .prepare("INSERT INTO suggestions (purpose, data) VALUES (?1, ?2)")
+    .bind(purpose, data)
+    .run();
+
+  console.log("DB res", res);
+};
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const { purpose } = body;
+  if (!purpose) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Purpose is required",
+    });
+  }
+
+  const data = await getAIService().getDomainSuggestions(purpose);
+
+  console.log("response from the AI service: ", data);
+
+  await saveSuggestions(purpose, data);
+
+  try {
+    return extractAndParseJson<DomainSuggestionsData>(data);
+  } catch (error) {
+    console.error("Error parsing JSON data:", error);
+    throw createError({
+      statusCode: 422,
+      statusMessage: "Failed to generate suggestions. Please try again.",
+    });
+  }
+});
