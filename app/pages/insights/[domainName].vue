9ba0858d924db0b0ae3f06ea08fa69d5867013d9
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-27 16:59:02 +0530

    feat: remove event handling from insights page

--- app/pages/insights/[domainName].vue
+++ app/pages/insights/[domainName].vue
@@ -3,11 +3,7 @@
     <DomainScoreLoader v-if="status === 'pending'" />
     <template v-else-if="insights">
       <div class="flex justify-center mb-6">
-        <DomainNameInput
-          :name="domainName"
-          class="w-full max-w-lg"
-          @name-input="onNameInput"
-        />
+        <DomainNameInput :name="domainName" class="w-full max-w-lg" />
       </div>
       <DomainScoreDisplay :score-data="insights" />
     </template>
@@ -20,8 +16,4 @@ const { domainName } = useRoute().params as { domainName: string };
 const { data: insights, status } = await useLazyFetch(
   `/api/insights/${domainName}`
 );
-
-const onNameInput = async (name: string) => {
-  navigateTo(`/insights/${name}`);
-};
 </script>
