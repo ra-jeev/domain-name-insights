0c845e8aad6f1b21342cb1f1e85c00833cd386de
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-27 16:59:25 +0530

    feat: new components AspectImage & ComparisonAspect

---
+++ app/components/ComparisonAspect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-start">
+    <div class="flex-shrink-0 mr-4">
+      <UIcon :name="icon" class="w-6 h-6 text-primary" />
+    </div>
+    <div>
+      <h3 class="text-lg font-semibold mb-2">{{ title }}</h3>
+      <p class="text-gray-600 dark:text-gray-300">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  icon: String,
+  title: String,
+  description: String,
+});
+</script>
