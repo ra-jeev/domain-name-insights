3e2752515756ddca43151b45e6717d2eb88cd074
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-28 12:04:01 +0530

    feat: minor refactor in winning domain card

--- app/components/WinningDomainCard.vue
+++ app/components/WinningDomainCard.vue
@@ -1,18 +1,23 @@
 <template>
   <UCard class="border-2 border-yellow-300">
-    <div class="flex items-center justify-center gap-x-4 md:gap-x-8">
+    <div class="flex items-center gap-x-4 md:gap-x-8">
       <UIcon
         name="i-heroicons-trophy-solid"
         class="w-20 h-20 text-yellow-300 shrink-0"
       />
       <div>
-        <h3 class="text-2xl font-bold text-gray-500 dark:text-gray-400 mb-2">
+        <h3 class="text-lg font-medium text-gray-500 dark:text-gray-400">
           Winner
         </h3>
         <p class="text-3xl font-extrabold text-primary">{{ winningDomain }}</p>
-        <p class="mt-4 text-gray-700 dark:text-gray-300">{{ explanation }}</p>
+        <p class="hidden sm:block mt-4 text-gray-700 dark:text-gray-300">
+          {{ explanation }}
+        </p>
       </div>
     </div>
+    <p class="sm:hidden mt-4 text-gray-700 dark:text-gray-300">
+      {{ explanation }}
+    </p>
   </UCard>
 </template>
 
