ced715cd2e757425add5099da4e9f8632b227363
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-16 22:05:19 +0530

    feat: support UI insights display from both AI services

--- app/components/DomainScoreDisplay.vue
+++ app/components/DomainScoreDisplay.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="space-y-6">
-    <OverallScoreCard :data="overallData" />
-    <ScoreLegend />
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-      <CategoryScoreCard
-        v-for="(category, categoryKey) in scoreData.categoryScores"
-        :key="categoryKey"
-        :category-name="DomainScoreCategory[categoryKey]"
-        :data="category"
-      />
-    </div>
+    <template v-if="scoreData">
+      <OverallScoreCard :data="overallData!" />
+      <ScoreLegend />
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <CategoryScoreCard
+          v-for="(category, categoryKey) in scoreData.categoryScores"
+          :key="categoryKey"
+          :category-name="DomainScoreCategory[categoryKey]"
+          :data="category"
+        />
+      </div>
+    </template>
+    <UCard v-else> Failed to generate insights. Please try again. </UCard>
   </div>
 </template>
 
@@ -18,14 +21,20 @@ import type { DomainScoreData } from "~~/types";
 import { DomainScoreCategory } from "~~/types";
 
 const props = defineProps<{
-  scoreData: DomainScoreData;
+  scoreData?: DomainScoreData;
 }>();
 
-const overallData = computed(() => ({
-  domainName: props.scoreData.domainName,
-  guessedPurpose: props.scoreData.guessedPurpose,
-  overallScore: props.scoreData.overallScore,
-  pros: props.scoreData.pros,
-  cons: props.scoreData.cons,
-}));
+const overallData = computed(() => {
+  if (props.scoreData) {
+    return {
+      domainName: props.scoreData.domainName,
+      guessedPurpose: props.scoreData.guessedPurpose,
+      overallScore: props.scoreData.overallScore,
+      pros: props.scoreData.pros,
+      cons: props.scoreData.cons,
+    };
+  }
+
+  return undefined;
+});
 </script>
--- app/pages/insights/[domainName].vue
+++ app/pages/insights/[domainName].vue
@@ -5,7 +5,15 @@
       <div class="flex justify-center mb-6">
         <DomainNameInput :name="domainName" class="w-full max-w-lg" />
       </div>
-      <DomainScoreDisplay :score-data="insights" />
+      <UTabs :items="items" class="w-full">
+        <template #item="{ item }">
+          <DomainScoreDisplay
+            :score-data="
+              item.key === 'anthropic' ? insights.anthropic : insights.hubAi
+            "
+          />
+        </template>
+      </UTabs>
     </template>
     <div v-else>Something went wrong</div>
   </UContainer>
@@ -16,4 +24,15 @@ const { domainName } = useRoute().params as { domainName: string };
 const { data: insights, status } = await useLazyFetch(
   `/api/insights/${domainName}`
 );
+
+const items = [
+  {
+    key: "anthropic",
+    label: "Claude 3.5 Sonnet",
+  },
+  {
+    key: "hubAi",
+    label: "Llama 3.1 8B",
+  },
+];
 </script>
